d6517715bbf1ddbc5c06a9c7bb459f2861668ffc
Vlad Cheliuskin <spawvn@gmail.com>
2020-08-08 13:16:37 +0200

    Updates

---
+++ precache-manifest.68dc8f8f4da706ba4bcb9ebf644c09df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f629e0368481d2cbc1475f64709f670c",
+    "url": "/pteranodon/index.html"
+  },
+  {
+    "revision": "ea3520a92085f5287f73",
+    "url": "/pteranodon/static/css/main.ea5f02f1.chunk.css"
+  },
+  {
+    "revision": "6eeab12656c996bb3e64",
+    "url": "/pteranodon/static/js/2.17f9b567.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/pteranodon/static/js/2.17f9b567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea3520a92085f5287f73",
+    "url": "/pteranodon/static/js/main.a54c4f74.chunk.js"
+  },
+  {
+    "revision": "da6675ede0191420d2e0",
+    "url": "/pteranodon/static/js/runtime-main.30f7014b.js"
+  }
+]);
